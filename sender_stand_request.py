cd5b04f865d80d465fc6cc471448f2fdfca374c1
stret <stret_rasers@mail.ru>
2024-11-30 19:52:33 +0500

    My first commit

---
+++ sender_stand_request.py
@@ -0,0 +1,32 @@
+import data
+import configuration
+import requests
+
+
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
+
+def post_new_user(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+                         json=body,
+                         headers=data.headers)
+
+def post_products_kits(body):
+    return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH,
+                         json=body,
+                         headers=data.headers)
+
+
+def post_new_client_kit(kit_body,auth_token):
+    auth_headers = data.headers.copy()
+    auth_headers["Authorization"] = "Bearer " + auth_token
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_KITS_PATH,
+                         json=kit_body,
+                         headers=auth_headers)
+
+
+
+
+#response = post_products_kits(data.product_ids);
+#print(response.status_code)
+#print(response.json())
